9c853df20324f7b8c90fabb0d6835cb785eea477
carlos <kikeztw@gmail.com>
2020-04-18 02:34:48 -0400

    move google review config to home page, fixed home page site data

--- src/pages/2020/index.js
+++ src/pages/2020/index.js
@@ -1,6 +1,7 @@
-import React, { PureComponent } from 'react';
-import Helmet from 'react-helmet';
+import React, { useEffect } from 'react';
 import get from 'lodash/get';
+import Helmet from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
 import { HeroHeader, HeroBody, Hero, Section } from 'bloomer';
 import Layout from '../../components/2020/Layout';
 import Header from '../../components/2020/Header';
@@ -12,65 +13,97 @@ import OurTeam from '../../components/2020/OurTeam';
 import Contact from '../../components/2020/HomePageContact';
 import TestimonialHomePage from '../../components/2020/TestimonialHomePage';
 import HomePagePost from '../../components/2020/HomePagePost';
+import { useScript } from '../../utils/hooks';
 import 'react-toastify/dist/ReactToastify.min.css';
 import 'react-modal-video/scss/modal-video.scss';
 import 'bulma';
 
-export default class HomePage extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showVideoModal: false,
+const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        description
+        title
+      }
+    }
+  }
+`;
+
+const HomePage = () => {
+  useScript('https://apis.google.com/js/platform.js?onload=renderBadge');
+  useScript('https://apis.google.com/js/platform.js?onload=renderOptIn');
+  const data = useStaticQuery(query);
+
+  useEffect(() => {
+    window.renderBadge = function() {
+      var ratingBadgeContainer = document.createElement('div');
+      document.body.appendChild(ratingBadgeContainer);
+      window.gapi.load('ratingbadge', function() {
+        window.gapi.ratingbadge.render(ratingBadgeContainer, {
+          merchant_id: process.env.MERCHANT_ID,
+          position: 'BOTTOM_RIGHT',
+        });
+      });
     };
-    this.openModal = this.openModal.bind(this);
-  }
 
-  openModal() {
-    this.setState({
-      showVideoModal: true,
-    });
-  }
+    window.renderOptIn = function() {
+      window.gapi.load('surveyoptin', function() {
+        window.gapi.surveyoptin.render({
+          merchant_id: process.env.MERCHANT_ID,
+          order_id: process.env.ORDER_ID,
+          email: process.env.EMAIL,
+          delivery_country: process.env.DELIVERY_CONTRY,
+          estimated_delivery_date: process.env.ESTIMATED_DELIVERY_DATE,
+        });
+      });
+    };
 
-  render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title');
-    const siteDescription = get(this, 'props.data.site.siteMetadata.description');
-    return (
-      <Layout>
-        <Helmet
-          htmlAttributes={{ lang: 'en' }}
-          meta={[{ name: 'description', content: siteDescription }]}
-          title={siteTitle}
-        />
-        <Hero>
-          <HeroHeader>
-            <Header />
-          </HeroHeader>
-          <HeroBody>
-            <HeroBodyContent />
-          </HeroBody>
-        </Hero>
-        <Section>
-          <Service />
-        </Section>
-        <Section>
-          <BusinessValue />
-        </Section>
-        <Section>
-          <HomePageCustomerSuccessStories />
-        </Section>
-        <Section isPaddingless>
-          <OurTeam />
-        </Section>
-        <Section isPaddingless>
-          <TestimonialHomePage />
-        </Section>
-        <Section>
-          <HomePagePost />
-        </Section>
-        <Section>
-          <Contact />
-        </Section>
-      </Layout>
-    );
-  }
-}
+    window.___gcfg = {
+      lang: 'en_US',
+    };
+  }, []);
+
+  const title = get(data, 'site.siteMetadata.title');
+  const description = get(data, 'site.siteMetadata.description');
+
+  return (
+    <Layout>
+      <Helmet
+        htmlAttributes={{ lang: 'en' }}
+        meta={[{ name: 'description', content: description }]}
+        title={title}
+      />
+      <Hero>
+        <HeroHeader>
+          <Header />
+        </HeroHeader>
+        <HeroBody>
+          <HeroBodyContent />
+        </HeroBody>
+      </Hero>
+      <Section>
+        <Service />
+      </Section>
+      <Section>
+        <BusinessValue />
+      </Section>
+      <Section>
+        <HomePageCustomerSuccessStories />
+      </Section>
+      <Section isPaddingless>
+        <OurTeam />
+      </Section>
+      <Section isPaddingless>
+        <TestimonialHomePage />
+      </Section>
+      <Section>
+        <HomePagePost />
+      </Section>
+      <Section>
+        <Contact />
+      </Section>
+    </Layout>
+  );
+};
+
+export default HomePage;
